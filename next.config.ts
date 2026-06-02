a10a8d747202fb0dfbde7194fe5bf08390becf38
wididikyy <wididiky38@gmail.com>
2026-06-02 21:30:09 +0700

    feat: image optimization and change metadataBase url

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
