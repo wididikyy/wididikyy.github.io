de4fe3162c2bc739d9ac60d5a029d53956bd8163
wididikyy <wididiky38@gmail.com>
2026-06-12 14:00:03 +0700

    refactor: remove border on toggle mode

--- components/toggle-mode.tsx
+++ components/toggle-mode.tsx
@@ -19,7 +19,7 @@ export function ToggleMode() {
       pressed={mounted ? resolvedTheme === "dark" : false}
       onPressedChange={(pressed) => setTheme(pressed ? "dark" : "light")}
       aria-label="Toggle theme"
-      className="size-9 rounded-full border-foreground dark:border-foreground"
+      className="size-9 border-0 bg-transparent!"
     >
       <RiSunFill className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <RiMoonFill className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
